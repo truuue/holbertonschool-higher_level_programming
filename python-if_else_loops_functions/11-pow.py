76dd174f3dfc0010a1f6a382e5991d3a47ff2620
truuue <noahvernhet@gmail.com>
2023-06-01 01:26:14 -0700

    another test for pow func

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def pow(a, b):
-    result = 1
-    for _ in range(b):
-        result *= a
-    return result
+    if b == 0:
+        return 1
+    elif b < 0:
+        return 1 / pow(a, -b)
+    else:
+        result = 1
+        for _ in range(b):
+            result *= a
+        return result
