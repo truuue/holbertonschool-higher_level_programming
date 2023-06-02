d38eb15210fabaaaa5c0ba1bfaea541c70f0ead3
truuue <noahvernhet@gmail.com>
2023-06-02 10:19:54 -0700

    switch to an older version

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,8 +6,6 @@ if __name__ == "__main__":
 
     if argc == 1:
         print("0 arguments.")
-    elif argc == 2:
-        print("1 arguments:")
     else:
         print("{} arguments:".format(argc - 1))
 
