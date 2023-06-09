256c5c89a72290ce4ecfac48475dc36d0d5eb7ce
truuue <noahvernhet@gmail.com>
2023-06-09 01:21:49 -0700

    Fix pycodestyle max int task

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,10 +2,10 @@
 def max_integer(my_list):
     if not my_list:
         return None
-    
+
     max_value = my_list[0]
     for num in my_list:
         if num > max_value:
             max_value = num
-    
+
     return max_value
