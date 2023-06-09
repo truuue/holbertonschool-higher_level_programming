bd8025f3c47f78cfad3fa5e9d55f9aa200961cfa
truuue <noahvernhet@gmail.com>
2023-06-09 13:46:31 -0700

    Try to fix the last checker

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0
     try:
         for i in range(x):
-            if type(my_list[i]) == int:
+            if isinstance(my_list[i], int):
                 print("{:d}".format(my_list[i]), end="")
-                count += 1
+                x += 1
         print()
-    except IndexError:
+    except (IndexError, TypeError):
         pass
-    return count
+    return x
