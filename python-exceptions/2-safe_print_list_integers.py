731c34bac1aa569fdcfb4547538dd4515e92e8e8
truuue <noahvernhet@gmail.com>
2023-06-09 13:52:39 -0700

    revert change and test to remove the counter

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,10 +2,10 @@
 def safe_print_list_integers(my_list=[], x=0):
     try:
         for i in range(x):
-            if isinstance(my_list[i], int):
+            if type(my_list[i]) == int:
                 print("{:d}".format(my_list[i]), end="")
                 x += 1
         print()
-    except (IndexError, TypeError):
+    except IndexError:
         pass
     return x
