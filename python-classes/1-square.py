51ff293115f4905210f82a6d160097d7be01e194
truuue <noahvernhet@gmail.com>
2023-06-14 01:09:29 -0700

    Docstrings fix

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""This module represent a square shape"""
 class Square:
+    """This is the Square class it attribute is __size for the square"""
     def __init__(self, size):
         self.__size = size
