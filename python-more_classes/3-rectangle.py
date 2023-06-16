7764b1f196314a85da715d5d62264fafe1748683
truuue <noahvernhet@gmail.com>
2023-06-16 04:22:31 -0700

    fix task 3

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -36,6 +36,6 @@ class Rectangle:
         return self.__width * self.__height
 
     def perimeter(self):
-        if self.__width == 0 or self.__height == 0:
+        if self.width == 0 or self.height == 0:
             return 0
         return (self.__width + self.__height) * 2
