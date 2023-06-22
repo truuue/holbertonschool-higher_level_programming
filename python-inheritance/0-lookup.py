ba33d8c392f16781569793f081503403429e7b7b
truuue <noahvernhet@gmail.com>
2023-06-22 13:38:31 -0700

    pycode style

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""That's a program for return a list of availables attributes"""
+"""That's a module for return a list of availables attributes"""
 def lookup(obj):
-    """This is the def"""
+    """This is the function"""
     return dir(obj)
