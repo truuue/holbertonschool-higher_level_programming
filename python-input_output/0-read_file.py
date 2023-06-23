6ca4c1bf588b61150a001fa6d03e5d409b5ca322
truuue <noahvernhet@gmail.com>
2023-06-23 07:55:56 -0700

    Read file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Simple function that reads a text file and print it to stdout"""
+
+def read_file(filename=""):
+    with open(filename, 'r', encoding='utf-8') as file:
+        print(file.read(), end="")
