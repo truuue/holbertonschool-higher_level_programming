dc6f9a5ff559c935e98146c66f37da77aac1d6d3
truuue <noahvernhet@gmail.com>
2023-06-23 07:58:39 -0700

    Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,4 +3,5 @@
 
 def read_file(filename=""):
     with open(filename, 'r', encoding='utf-8') as file:
-        print(file.read(), end="")
+        for line in file:
+            print(line, end="")
