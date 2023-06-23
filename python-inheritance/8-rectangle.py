f81ebbb901802abdcfe9a760573fa523f33cd468
truuue <noahvernhet@gmail.com>
2023-06-23 07:13:36 -0700

    Rectangle

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""A simple module for a basegeaometry"""
+
+
+class BaseGeometry:
+    """A geometry module"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    """Class that inherits from basegeometry"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
