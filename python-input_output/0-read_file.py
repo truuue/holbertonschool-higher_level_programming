e0f90083595df1563e2621bf2337f2263bf8422b
truuue <noahvernhet@gmail.com>
2023-06-24 06:05:33 -0700

    pycodestyle fix

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Simple module that reads a text file and print it to stdout"""
 
+
 def read_file(filename=""):
     """Function that reads a text file"""
     with open(filename, encoding="utf-8") as file:
