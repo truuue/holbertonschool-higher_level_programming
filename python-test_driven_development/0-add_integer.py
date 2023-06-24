6a33f49f0d9e27e0e86d73c43fa9105d4e90ea0c
truuue <noahvernhet@gmail.com>
2023-06-24 06:32:35 -0700

    Integers addition

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-"""Function that add two integer"""
+"""Simple module that add two integer"""
 
 
 def add_integer(a, b=98):
-    """here is the function that print the value or print the error"""
-    if a is isinstance(a, int) or a is isinstance(a, float):
-        return int(a) + int(b)
+    """This function adds two integers"""
+    if (type(a) is int or type(a) is float):
+        if (type(b) is int or type(b) is float):
+            return int(a) + int(b)
+        else:
+            raise TypeError("b must be an integer")
     else:
         raise TypeError("a must be an integer")
-
-    if b is isinstance(b, int) or b is isinstance(b, float):
-        return int(a) + int(b)
-    else:
-        raise TypeError("b must be an integer")
