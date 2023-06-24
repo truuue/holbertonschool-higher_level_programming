70cf5ae38b50decf1689cdde060d6fc646b50e59
truuue <noahvernhet@gmail.com>
2023-06-24 06:09:42 -0700

    Append to a file

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Simple module that reads a text file and print it to stdout"""
+
+
+def append_write(filename="", text=""):
+    """Function that reads a text file"""
+    with open(filename, "a",encoding="utf-8") as file:
+            return file.write(text)
