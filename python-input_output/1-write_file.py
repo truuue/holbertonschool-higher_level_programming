d6ad8ee6077668901b1e8ed89699cf117b596e27
truuue <noahvernhet@gmail.com>
2023-06-24 06:12:29 -0700

    indentation fix

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,4 +5,4 @@
 def write_file(filename="", text=""):
     """Function that reads a text file"""
     with open(filename, 'w',encoding="utf-8") as file:
-            return file.write(text)
+        return file.write(text)
