f5665d7527372d0633ac0c4f7210463bf0cac289
truuue <noahvernhet@gmail.com>
2023-06-24 06:08:29 -0700

    Write to a file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Simple module that reads a text file and print it to stdout"""
+
+
+def write_file(filename="", text=""):
+    """Function that reads a text file"""
+    with open(filename, "w",encoding="utf-8") as file:
+            return file.write(text)
