207a181fb11f04c9fd3003344a70617aad3ee086
truuue <noahvernhet@gmail.com>
2023-06-27 02:25:29 -0700

    Update #0 || #1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -82,3 +82,38 @@ class Rectangle(Base):
         """A simple function that use __str__ method"""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.x, self.y, self.width, self.height)
+
+    def update(self, *args, **kwargs):
+        """Update the Rectangle with args"""
+        if args and len(args) != 0:
+            argument = 0
+            for arg in args:
+                if argument == 0:
+                    if arg is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif argument == 1:
+                    self.width = arg
+                elif argument == 2:
+                    self.height = arg
+                elif argument == 3:
+                    self.x = arg
+                elif argument == 4:
+                    self.y = arg
+                argument += 1
+        elif kwargs and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "id":
+                    if value is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = value
+                elif key == "width":
+                    self.width = value
+                elif key == "height":
+                    self.height = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
