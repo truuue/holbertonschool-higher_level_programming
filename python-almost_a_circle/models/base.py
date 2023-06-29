f929d225faf474810dde2100ba87c192ba622c25
truuue <noahvernhet@gmail.com>
2023-06-29 01:10:59 -0700

    Dictionary to JSON string

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,13 +2,35 @@
 """A simple module who are the base of the project"""
 
 
+import json
+
 class Base:
-    """The base class"""
+    """Base class"""
+
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Initialize a Base instance"""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Save a list of objects to a file in JSON format"""
+        filename = cls.__name__ + ".json"
+        obj_list = []
+        if list_objs is not None:
+            obj_list = [obj.to_dictionary() for obj in list_objs]
+        json_str = cls.to_json_string(obj_list)
+        with open(filename, "w") as file:
+            file.write(json_str)
