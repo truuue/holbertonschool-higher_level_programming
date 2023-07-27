f1eaf56b8e61ffb45590cda2afc2c037ae4c3d68
truuue <noahvernhet@gmail.com>
2023-07-27 01:58:48 -0700

    Full creation

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+-- Script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows
+IF NOT EXIST CREATE TABLE second_table (
+    id INT,
+    name VARCHAN(256),
+    score INT
+);
