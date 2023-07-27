2ebe2ce5b41a62db136bdc5c03d7a68211418d4b
truuue <noahvernhet@gmail.com>
2023-07-27 02:10:58 -0700

    Full creation 7

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS second_table(
     score INT
 );
 
-INSERT INTO second_table VALUE(1, “John”, 10);
-INSERT INTO second_table VALUE(2, “Alex”, 3);
-INSERT INTO second_table VALUE(3, “Bob”, 14);
-INSERT INTO second_table VALUE(4, “George”, 8);
+INSERT INTO second_table VALUES(1, “John”, 10);
+INSERT INTO second_table VALUES(2, “Alex”, 3);
+INSERT INTO second_table VALUES(3, “Bob”, 14);
+INSERT INTO second_table VALUES(4, “George”, 8);
