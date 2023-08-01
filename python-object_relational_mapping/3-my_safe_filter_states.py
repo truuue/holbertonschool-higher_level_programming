42765d9e89cca4ed553c98a034febc3f2e437f20
truuue <noahvernhet@gmail.com>
2023-08-01 13:24:06 -0700

    Try another method

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python3
 
-"""Script that takes in an argument and
-displays all values in the states table of hbtn_0e_0_usa
-where name matches the argument"""
+"""A script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument"""
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    user_db = sys.argv[1]
-    passwd_db = sys.argv[2]
-    name_db = sys.argv[3]
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=user_db,
-        passwd=passwd_db,
-        db=name_db)
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
     cursor = db.cursor()
-    cursor.execute("""SELECT * FROM states WHERE name LIKE BINARY
-    %s ORDER BY id ASC""", (sys.argv[4],))
+    cursor.execute(
+        """
+        SELECT *
+        FROM states
+        WHERE states.name LIKE BINARY %s
+        ORDER BY states.id
+        """, (sys.argv[4],)
+    )
+
     rows = cursor.fetchall()
     for row in rows:
         print(row)
+
     cursor.close()
     db.close()
