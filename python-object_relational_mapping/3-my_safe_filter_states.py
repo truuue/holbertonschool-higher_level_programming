adca34ffedd09fa929e55b4e6bf40b2e92b4c384
truuue <noahvernhet@gmail.com>
2023-08-01 13:29:00 -0700

    Updated stuff

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,33 +1,31 @@
 #!/usr/bin/python3
 
-"""A script that takes in an argument and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument"""
+"""Script that takes in an argument and
+displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument"""
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
+    user_db = sys.argv[1]
+    passwd_db = sys.argv[2]
+    name_db = sys.argv[3]
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
-
+        user=user_db,
+        passwd=passwd_db,
+        db=name_db)
     cursor = db.cursor()
-    cursor.execute(
-        """
-        SELECT *
-        FROM states
-        WHERE states.name LIKE BINARY %s
-        ORDER BY states.id
-        """, (sys.argv[4],)
-    )
-
+    cursor.execute("""SELECT * FROM states WHERE name LIKE BINARY
+    %s ORDER BY id ASC""", (sys.argv[4],))
     rows = cursor.fetchall()
+    printed_states = set()
     for row in rows:
-        print(row)
-
+        state_id, state_name = row
+        if state_name not in printed_states:
+            print(row)
+            printed_states.add(state_name)
     cursor.close()
     db.close()
