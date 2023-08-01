9f109e53c2c164f287a7322cae69928259b09282
truuue <noahvernhet@gmail.com>
2023-08-01 13:08:05 -0700

    Another method for pass the checker

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 """Script that takes in an argument and
-displays all values in the states table of hbtn_0e_0_usawhere name matches the argument"""
+displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument"""
 
 import MySQLdb
 import sys
@@ -10,7 +11,6 @@ if __name__ == "__main__":
     user_db = sys.argv[1]
     passwd_db = sys.argv[2]
     name_db = sys.argv[3]
-    state_name = sys.argv[4]
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -18,10 +18,8 @@ if __name__ == "__main__":
         passwd=passwd_db,
         db=name_db)
     cursor = db.cursor()
-    query = """SELECT * FROM states
-        WHERE states.name LIKE BINARY '{}'
-        ORDER BY states.id""".format(state_name)
-    cursor.execute(query)
+    cursor.execute("""SELECT * FROM states WHERE name LIKE BINARY
+    '{}' ORDER BY id ASC""".format(sys.argv[4]))
     rows = cursor.fetchall()
     printed_states = set()
     for row in rows:
