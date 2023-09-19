527122291ec3afd2565089264e770517718a39df
truuue <noahvernhet@gmail.com>
2023-09-19 04:55:46 -0700

    Toggle classes

---
+++ javascript-web_jquery/4-script.js
@@ -0,0 +1,8 @@
+$('#toggle_header').click(function toggleHeaderClass() {
+  const header = $('header');
+  if (header.hasClass('red')) {
+      header.removeClass('red').addClass('green');
+  } else {
+      header.removeClass('green').addClass('red');
+  }
+});
